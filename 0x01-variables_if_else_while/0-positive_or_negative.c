b01c9b3fde867a3c34038cd7bf2e09be05b5dd82
Robsen Gelana <robsengelana21@gmail.com>
2022-04-08 07:03:40 -0700

    removing extra else if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,12 @@ int main(void)
 	if (n > 0)
 	{
 		printf("%i, is positive\n", n);
-	} else if (n < 0)
-	{
-		printf("%i, is negative\n", n);
 	} else if (n == 0)
 	{
 		printf("%i, is zero\n", n);
 	} else
 	{
-		printf("Invalid number");
+		printf("%i, is negative\n", n);
 	}
 	return (0);
 }

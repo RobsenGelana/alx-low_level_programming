4368411310f46aaa0f0b719014e69a7f693204e4
Robsen Gelana <robsengelana21@gmail.com>
2022-04-08 11:56:51 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *My first logial programm
+ *Return: Always 0 after program executaion
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+}
+putchar("\n");
+return (0);
+}

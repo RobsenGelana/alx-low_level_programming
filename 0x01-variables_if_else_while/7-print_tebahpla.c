7e6f9c09249e2b71ae7a43c1fc634af1b32a4dd8
Robsen Gelana <robsengelana21@gmail.com>
2022-04-08 12:10:49 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+
+ *Return: Always 0 after program executaion
+ */
+
+int main(void)
+{
+char lower_case;
+
+for (lower-case = 122; lower_case >= 97; lower_case--)
+{
+putchar(lower_case);
+}
+putchar('\n');
+return (0);
+}

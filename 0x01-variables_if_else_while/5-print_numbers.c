e3035747496c02173251464c2d8eb34f9a737684
Robsen Gelana <robsengelana21@gmail.com>
2022-04-08 11:43:16 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 after code execute
+ */
+
+int main(void)
+{
+int single_digit;
+for (single_digit = 0; single_digit < 10; single_digit++)
+{
+printf("%i\n", single_digit);
+}
+return (0);
+}

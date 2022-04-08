49a43d04e426cd0d3a93198db0e29904573f80c8
Robsen Gelana <robsengelana21@gmail.com>
2022-04-08 10:45:13 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 after code execute
+ */
+char variable[] = "abcdefghijklmnopqrstuvwxyz";
+int main(void)
+{
+putchar(variable);
+return (0);
+}

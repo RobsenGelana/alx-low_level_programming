abb1122bb0c479d016ec17754d4ad6a0ad84e9c8
Robsen Gelana <robsengelana21@gmail.com>
2022-04-08 11:36:30 -0700

    fixing problems

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,16 +8,18 @@ int main(void)
 {
 char lower_case;
 
-switch (lower_case)
+for(lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
-case 'q':
-break;
-case 'e':
-break;
-default:
+if(lower_case == 'q')
+{
+continue;
+}
+else if (lower_case == 'e')
+{
+continue;
+}
 putchar(lower_case);
 }
 putchar('\n');
-
 return (0);
 }

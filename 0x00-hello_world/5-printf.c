d66e89147fadd5e214b3cff0c4309b15275d311d
Robsen Gelana <robsengelana21@gmail.com>
2022-04-07 23:53:39 -0700

    first in c task 5 second fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *main - entry point
- *Return: 0 function
+ *main - Entry point
+ *My hello, printf task
+ *Return: return 1 after program executaion
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }

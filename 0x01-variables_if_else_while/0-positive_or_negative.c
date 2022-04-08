f7d0d9b832311ba33ea08dd5447aa6a67e3b4f4b
Robsen Gelana <robsengelana21@gmail.com>
2022-04-08 07:32:30 -0700

    working guess

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 /**
- *main - Entry point
- *My first logial programm
- *Return: Always 0 after program executaion
- */
+ * main - Entry point
+ * Return: Always 0 after a programm execution
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -16,16 +14,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n == 0)
-        {
-		printf("%i, is zero\n", n);
-	}
-	else if (n<0)
-	{ 
-		printf("%i, is negative\n", n);
-  	}
-	else
 	{
-		printf("%i, is positive\n", n);
+		printf("%i is zero\n");
+	} else if (n < 0)
+	{
+		printf("%i is negative\n");
+	} else {
+		printf("%i is positive\n");
 	}
 	return (0);
-}    
+}

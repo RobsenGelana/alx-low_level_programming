cb1bdf7360ebc81839e521f63fd65e3b7cfcb96a
Robsen Gelana <robsengelana21@gmail.com>
2022-04-08 11:24:56 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 after code execute
+ */
+
+int main(void)
+{
+char lower_case;
+
+switch (lower_case)
+{
+case q:
+break;
+case e:
+break;
+default;
+putchar(lower_case);
+}
+putchar('\n');
+
+return (0);
+}

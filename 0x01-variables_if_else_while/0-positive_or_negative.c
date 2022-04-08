f9b511628b4a626c10a398adbfad2ec6b6918935
Robsen Gelana <robsengelana21@gmail.com>
2022-04-08 07:00:07 -0700

    chaning %d to %i

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,13 +17,13 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("%d, is positive", n);
+		printf("%i, is positive\n", n);
 	} else if (n < 0)
 	{
-		printf("%d, is negative", n);
+		printf("%i, is negative\n", n);
 	} else if (n == 0)
 	{
-		printf("%d, is zero", n);
+		printf("%i, is zero\n", n);
 	} else
 	{
 		printf("Invalid number");

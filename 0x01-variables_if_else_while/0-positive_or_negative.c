e6340cf95987cdeaad0e82ad4d79bc47d3de713e
Robsen Gelana <robsengelana21@gmail.com>
2022-04-08 06:11:18 -0700

    my first c logical code

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/* This code should return positive or negative number */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if(n > 0){
+		printf("%d, is positive", n);
+	}else if(n = o){
+		printf("%d, is negative", n);
+	}else if(n > 0){
+		printf("%d, is zero", n);
+	}
+	return (0);
+}

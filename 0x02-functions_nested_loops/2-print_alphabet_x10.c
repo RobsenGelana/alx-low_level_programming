0caf4eeaeef26f8e57084dac2bb354f642048c6c
Robsen Gelana <robsengelana21@gmail.com>
2022-04-12 04:01:12 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 after code execute
+ */
+
+void print_alphabet_x10(void)
+{
+int n, i;
+
+i = 0;
+
+while (i < 10)
+{
+
+for (n = 'a'; n <= 'z'; n++)
+{
+
+_puthcar(n);
+
+}
+
+i++;
+
+_putchar('\n');
+
+}
+
+return (0);
+
+}

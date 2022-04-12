23c57f1a51b4fb9df335ce7e921758120d262e04
Robsen Gelana <robsengelana21@gmail.com>
2022-04-12 03:53:06 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h">
+/**
+ * main - Entry point
+ *Return: Always return 0 after code execute
+ */
+
+void print_alphabet(void);
+int main(void)
+{	
+int n;
+
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
+
+}
+
+return (0);
+
+}

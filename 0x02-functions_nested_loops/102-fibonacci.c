5ba60d8ef4f96b34511cfe4a3638d12a5cae66af
Robsen Gelana <robsengelana21@gmail.com>
2022-04-13 10:37:50 -0700

    advanced task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 after code execute
+ */
+
+int main(void)
+{
+unsigned long count, i, j, k;
+
+i = 0;
+j = 1;
+for (count = 0; count < 50; count++)
+{
+
+k = i + j;
+i = j;
+j = k;
+printf("%lu", k);
+if (count == 49)
+putchar('\n');
+else
+{
+printf(", ");
+}
+
+}
+return (0);
+
+}

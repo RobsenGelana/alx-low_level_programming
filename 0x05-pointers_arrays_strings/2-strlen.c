7f6cc3c790aaf226cb49b9c6a364c9906267372c
Robsen Gelana <robsengelana21@gmail.com>
2022-04-18 05:02:15 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - Entry point
+ *return: always after code execute
+ */
+
+int _strlen(char *s);
+{
+
+int l;
+
+l = 0;
+while (s[l] != '\0')
+{
+l++;
+
+}
+
+return (l);
+
+}

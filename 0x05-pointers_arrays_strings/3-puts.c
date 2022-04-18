eb6cba2952342854473b4a50366343aef738067a
Robsen Gelana <robsengelana21@gmail.com>
2022-04-18 05:06:04 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ main - Entry point
+ return - 0 after code execute
+ */
+
+void _puts(char *str);
+{
+char *a;
+int b;
+
+a = str;
+
+for (b = 0; a[b]; b++)
+{
+
+_putchar (a[b]);
+}
+
+_putchar('\n');
+}

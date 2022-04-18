e8e9c11c681bd5321a3b1c3cf7e556e453b5511c
Robsen Gelana <robsengelana21@gmail.com>
2022-04-18 04:58:27 -0700

    pointer project task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include "main.h>
+
+/**
+ *main - Entry point
+ *Return: Alwasy 0 after code execute
+ */
+
+void swap_int(int *a, int *b);
+{
+
+int c;
+
+c = *a;
+*a = *b;
+*b = c;
+}

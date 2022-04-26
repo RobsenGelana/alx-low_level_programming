238b9ed456250426557fc769fba1fe1a582a133a
Robsen Gelana <robsengelana21@gmail.com>
2022-04-26 00:27:10 -0700

    even more pointer task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *Return: dest
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(r + i) = s; 
+
+	} 
+
+	return (r);
+}

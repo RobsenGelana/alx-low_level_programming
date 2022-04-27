46f53cd0c49a0d6116df4358dd9870d45f7c7c2a
Robsen Gelana <robsengelana21@gmail.com>
2022-04-27 04:02:55 -0700

    recursion task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * main - Entry point
+ * _print_rev_recursion - prints a string in reverse
+ * Return: Always 0 or void
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+if (*s)//Pointing it to the pointer
+{
+_print_rev_recursion(s + 1);//If the condition is true add 1 
+_putchar(*s);//then print *s
+}
+}
+
